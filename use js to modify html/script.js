d713795d74a7b0dfcc3afd9446da284bf6fb6f4e
Owen <900920ya@gmail.com>
2022-04-20 21:09:40 +0800

    finish all

--- use js to modify html/script.js
+++ use js to modify html/script.js
@@ -1,7 +1,22 @@
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
+const content = document.createElement('p');
 content.setAttribute('style', 'color: red;');
 content.textContent = "Hey I'm red!";
+container.appendChild(content);
 
-container.appendChild(content)
+const p3 = document.createElement('h3');
+p3.setAttribute('style', 'color: blue;');
+p3.textContent = "I'm blue h3!";
+container.appendChild(p3);
+
+const div = document.createElement('div');
+div.setAttribute('id', 'newDiv');
+container.appendChild(div);
+const newDiv = document.querySelector('#newDiv');
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div";
+newDiv.appendChild(h1);
+const p = document.createElement('p');
+p.textContent = "ME TOO!";
+newDiv.appendChild(p);
